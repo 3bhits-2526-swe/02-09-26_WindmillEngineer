57390d5cec2ac16f71f86282280265e06eb2c6a3
fabian-wolf09 <fabs.wolf44@gmail.com>
2026-05-27 10:17:33 +0200

    Ordnerstruktur, Balken angelegt

---
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Image demandBar;
+    public Image supplyBar;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
