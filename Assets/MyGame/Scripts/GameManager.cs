86d9a7d15441003c2c3e10167de3256d8d1fd862
fabian-wolf09 <fabs.wolf44@gmail.com>
2026-05-29 12:22:47 +0200

    Added Farbverlauf material for Supply and Demand, Added GameManager Script to Gamemanager Object

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Image demandBar;
     public Image supplyBar;
+    public 
     void Start()
     {
         
