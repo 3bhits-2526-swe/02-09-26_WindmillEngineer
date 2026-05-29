78f72e284fa8949eeaf117dea8713c2be508a379
fabian-wolf09 <fabs.wolf44@gmail.com>
2026-05-29 12:55:05 +0200

    Added behavement of demand bar

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -1,20 +1,44 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Configurable Variables")]
+
+    public float demandTimerMax;
+
+
+
+    [Header("UI Bars")]
     public Image demandBar;
     public Image supplyBar;
-    public 
+    public Image batteryBar;
+    [Header("Other Variables")]
+    public float demandTimer;
+
+
     void Start()
     {
-        
+        CalcDemand();
     }
 
     // Update is called once per frame
     void Update()
     {
+        CalcDemand();        
+    }
+    
+    public void CalcDemand()
+    {
+        demandTimer += Time.deltaTime;
+        if (demandTimer >= demandTimerMax)
+        {
+            float demand = 0;
+            demand = UnityEngine.Random.Range(0f, 1f);
+            demandBar.fillAmount = demand;
+            demandTimer = 0f;
+        }
         
     }
 }
