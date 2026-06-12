a4abb1125fc0b2c33f3ea191c0c2dbed6edd0890
Nirual09 <101121294+Nirual09@users.noreply.github.com>
2026-06-12 13:38:57 +0200

    Added Akku funktionality
    Akku can now go up (charge) and go down (discharge)

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -5,32 +5,53 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Configurable Variables")]
-
+    [Header("Demand")]
     public float demandTimerMax;
-
-
-
-    [Header("UI")]
+    public float demandTimer;
     public Image demandBar;
+
+    [Header("Wind")]
+    public float windTimerMax;
+    public Image windBar;
+    public float windTimer;
+    public Vector2 windLevel;
+
+    [Header("Windmill")]
+    public int activWindmills = 1;
+
+    [Header("Supply")]
     public Image supplyBar;
-    public Image batteryBar;
+
+    [Header("Akku")]
+    public Image AkkuBar;
+    public float currentAkku = 45f;
+    public float AkkuDischargeSpeed = 2.25f;
+    public float standardAkkuChargeSpeed = 1.50f;
+    
+    [Header("Money")]
+    public float money;
     public TextMeshProUGUI moneyText;
 
-    [Header("Other Variables")]
-    public float demandTimer;
-    public int money;
-
+    [Header("Screen")]
+    public GameObject gameScreen;
+    public GameObject winScreen;
+    public GameObject loseScreen;
 
     void Start()
     {
-        CalcDemand();
+        demandTimer = demandTimerMax;
+        windTimer = windTimerMax;
+        
+        InvokeRepeating(nameof(UpdateAkku), 0f, 1f);
+
+        HideScreen(winScreen);
+        HideScreen(loseScreen);
     }
 
-    // Update is called once per frame
     void Update()
     {
         CalcDemand();
+        CalcWind();
         RenderMoney();
     }
     
@@ -39,25 +60,68 @@ public class GameManager : MonoBehaviour
         demandTimer += Time.deltaTime;
         if (demandTimer >= demandTimerMax)
         {
-            float demand = 0;
-            demand = UnityEngine.Random.Range(0f, 1f);
+            float demand = UnityEngine.Random.Range(0f, 1f);
             demandBar.fillAmount = demand;
             demandTimer = 0f;
         }
-        
     }
+
     public void RenderMoney()
     {
         moneyText.text = "$" + money.ToString();
     }
+
     public void CalcWind()
     {
         windTimer += Time.deltaTime;
         if (windTimer >= windTimerMax)
         {
-            windLevel = UnityEngine.Random.insideUnitCircle.normalized;
+            windLevel = UnityEngine.Random.insideUnitCircle;
             windBar.fillAmount = (Mathf.Abs(windLevel.x) + Mathf.Abs(windLevel.y)) / 2f;
             windTimer = 0f;
+        }
+    }
+
+    public void UpdateAkku()
+    {
+        currentAkku -= AkkuDischargeSpeed;
+        
+        float windStrength = (Mathf.Abs(windLevel.x) + Mathf.Abs(windLevel.y)) / 2f;
+        currentAkku += activWindmills * standardAkkuChargeSpeed * windStrength;
+
+        money += 12.8f;
+
+        AkkuBar.fillAmount = currentAkku / 100f;
+
+        if (currentAkku >= 100f)
+        {
+            currentAkku = 100f;
+            HideScreen(gameScreen);
+            ShowScreen(winScreen);
+            CancelInvoke(nameof(UpdateAkku));
+        }
+        else if (currentAkku <= 0f)
+        {
+            currentAkku = 0f;
+            HideScreen(gameScreen);
+            ShowScreen(loseScreen);
+            CancelInvoke(nameof(UpdateAkku));
+        }
+    }
+
+    public void ShowScreen(GameObject screen)
+    {
+        if (screen != null)
+        {
+            screen.SetActive(true);
+        }
+    }
+
+    public void HideScreen(GameObject screen)
+    {
+        if (screen != null)
+        {
+            screen.SetActive(false);
         }
     }
 }
