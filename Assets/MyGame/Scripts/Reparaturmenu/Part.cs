084d3781c5eba2779be279ca4b7bcb7d6473b0ff
Wekúr <manuel123auer@gmail.com>
2026-06-19 23:11:53 +0200

    publish for testing

--- Assets/MyGame/Scripts/Reparaturmenu/Part.cs
+++ Assets/MyGame/Scripts/Reparaturmenu/Part.cs
@@ -2,17 +2,34 @@ using UnityEngine;
 
 public class Part : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	int number;
+	Vector2 startPos;
+	void Start(){
+		//init sprite
+		startPos = this.transform.position;
+		Debug.Log("part init"); 
 	}
-	
+	// Use this for initialization
 	// Update is called once per frame
-	void Update () {
-        
+	
+	void Update(){
+		Debug.Log("screen pos:   x: " + Input.mousePosition.x + "y: " + Input.mousePosition.y); 
 	}
 
-	void OnMouseDrag(){
-		this.gameObject.transform.position;
+	void OnMouseOver(){
+		Debug.Log("this part could be selected"); 
+	}
+
+    void OnMouseDrag()
+    {
+		Vector3 input = Input.mousePosition; 
+		Vector3 offset = Camera.main.transform.position; 
+		input.x += -860 + offset.x; 
+		input.y += -540 + offset.y; 
+    	Debug.Log("chosen pos[x: "+ input.x + "; y: " + input.y + "; z: " + input.z); 
+	}
+
+	void OnMouseExit(){
+		this.transform.position = startPos;
 	}
 }
--- Assets/MyGame/Scripts/Windmill.cs
+++ Assets/MyGame/Scripts/Windmill.cs
@@ -1,13 +1,25 @@
 using UnityEngine; 
+
 public class Windmill : MonoBehaviour {
+	
+	bool broken = false; 
+
+	float windStrength = 0f; 
+	GameObject gm; 
+
 
 	// Use this for initialization
 	void Start () {
-	
+		//pulling config
+		MonoBehaviour gm = GameObject.Find("GameManager").GetComponent<Part>();
+		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
         
 	}
+
+
 }
