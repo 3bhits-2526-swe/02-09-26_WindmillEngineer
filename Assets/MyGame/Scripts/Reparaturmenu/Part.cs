e4f9bee4c54d4eb2bfca74ba7ce202ba9b6a9361
Wekúr <manuel123auer@gmail.com>
2026-06-19 10:56:32 +0200

    first steps for reperaturment
    added assets for windmills + diff states; started code; gui created, yet to be made functional

---
+++ Assets/MyGame/Scripts/Reparaturmenu/Part.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Part : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+	void OnMouseDrag(){
+		this.gameObject.transform.position;
+	}
+}
---
+++ Assets/MyGame/Scripts/Windmill.cs
@@ -0,0 +1,13 @@
+using UnityEngine; 
+public class Windmill : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
